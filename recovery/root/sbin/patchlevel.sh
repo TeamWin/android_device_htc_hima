5da36b0b5578a880e820686c63d1f6c3b0808152
Captain Throwback <captainthrowback@hotmail.com>
2019-07-03 10:07:46 -0400

    hima: clean up decrypt scripts, kernel and blobs
    - Update patchlevel script to only run when device is
      encrypted
    - Relink blobs in script instead of in device files
    - Allow setting device and fingerprint props to be configurable
    - Remove unnecessary servicemanager service
    - Update kernel to include f2fs support and appended dtb
    
    Change-Id: I58551b26bb9d6cde84d2df34690e3b57c16edd7c

--- recovery/root/sbin/patchlevel.sh
+++ recovery/root/sbin/patchlevel.sh
@@ -6,6 +6,9 @@ TEMPSYS=/s
 BUILDPROP=build.prop
 DEFAULTPROP=prop.default
 syspath="/dev/block/bootdevice/by-name/system"
+venbin="/vendor/bin"
+SETDEVICE=false
+SETFINGERPRINT=true
 
 log_info()
 {
@@ -17,12 +20,44 @@ log_error()
 	echo "E:$SCRIPTNAME:$1" >> "$LOGFILE"
 }
 
+temp_mount()
+{
+	mkdir "$1"
+	if [ -d "$1" ]; then
+		log_info "Temporary $2 folder created at $1."
+	else
+		log_error "Unable to create temporary $2 folder."
+		finish_error
+	fi
+	mount -t ext4 -o ro "$3" "$1"
+	if [ -n "$(ls -A "$1" 2>/dev/null)" ]; then
+		log_info "$2 mounted at $1."
+	else
+		log_error "Unable to mount $2 to temporary folder."
+		finish_error
+	fi
+}
+
+relink()
+{
+	log_info "Looking for $1 to update linker path..."
+	if [ -f "$1" ]; then
+		fname=$(basename "$1")
+		target="/sbin/$fname"
+		log_info "File found! Relinking $1 to $target..."
+		sed 's|/system/bin/linker|///////sbin/linker|' "$1" > "$target"
+		chmod 755 "$target"
+	else
+		log_info "File not found. Proceeding without relinking..."
+	fi
+}
+
 finish()
 {
 	umount "$TEMPSYS"
 	rmdir "$TEMPSYS"
-	setprop fde.ready 1
-	log_info "fde.ready=$(getprop fde.ready)"
+	setprop crypto.ready 1
+	log_info "crypto.ready=$(getprop crypto.ready)"
 	log_info "Script complete. Device ready for decryption."
 	exit 0
 }
@@ -44,61 +79,62 @@ fingerprint=$(getprop ro.build.fingerprint)
 product=$(getprop ro.build.product)
 
 log_info "Running patchlevel pre-decrypt script for TWRP..."
+relink "$venbin/qseecomd"
+relink "$venbin/hw/android.hardware.keymaster@3.0-service"
 
-mkdir "$TEMPSYS"
-if [ -d "$TEMPSYS" ]; then
-	log_info "Temporary system folder created at $TEMPSYS."
-else
-	log_error "Unable to create temporary system folder."
-	finish_error
-fi
-mount -t ext4 -o ro "$syspath" "$TEMPSYS"
-if [ -n "$(ls -A "$TEMPSYS" 2>/dev/null)" ]; then
-	log_info "System mounted at $TEMPSYS."
-else
-	log_error "Unable to mount system to temporary folder."
-	finish_error
-fi
+temp_mount "$TEMPSYS" "system" "$syspath"
 
 if [ -f "$TEMPSYS/$BUILDPROP" ]; then
-	log_info "Build.prop exists! Setting system properties from build.prop"
-	# TODO: It may be better to try to read these from the boot image than from /system
-	log_info "Current OS version: $osver"
-	osver=$(grep -i 'ro.build.version.release' "$TEMPSYS/$BUILDPROP"  | cut -f2 -d'=' -s)
-	if [ -n "$osver" ]; then
-		resetprop ro.build.version.release "$osver"
-		sed -i "s/ro.build.version.release=.*/ro.build.version.release=""$osver""/g" "/$DEFAULTPROP" ;
-		log_info "New OS Version: $osver"
-	fi
-	log_info "Current security patch level: $patchlevel"
-	patchlevel=$(grep -i 'ro.build.version.security_patch' "$TEMPSYS/$BUILDPROP"  | cut -f2 -d'=' -s)
-	if [ -n "$patchlevel" ]; then
-		resetprop ro.build.version.security_patch "$patchlevel"
-		sed -i "s/ro.build.version.security_patch=.*/ro.build.version.security_patch=""$patchlevel""/g" "/$DEFAULTPROP" ;
-		log_info "New security patch level: $patchlevel"
+	sdkver=$(grep -i 'ro.build.version.sdk' "$TEMPSYS/$BUILDPROP"  | cut -f2 -d'=' -s)
+	log_info "Current system Android SDK version: $sdkver"
+	if [ "$sdkver" -gt 25 ]; then
+		log_info "Current system is Oreo or above. Proceed with setting OS version and security patch level..."
+		log_info "Build.prop exists! Setting system properties from build.prop"
+		# TODO: It may be better to try to read these from the boot image than from /system
+		log_info "Current OS version: $osver"
+		osver=$(grep -i 'ro.build.version.release' "$TEMPSYS/$BUILDPROP"  | cut -f2 -d'=' -s)
+		if [ -n "$osver" ]; then
+			resetprop ro.build.version.release "$osver"
+			sed -i "s/ro.build.version.release=.*/ro.build.version.release=""$osver""/g" "/$DEFAULTPROP" ;
+			log_info "New OS Version: $osver"
+		fi
+		log_info "Current security patch level: $patchlevel"
+		patchlevel=$(grep -i 'ro.build.version.security_patch' "$TEMPSYS/$BUILDPROP"  | cut -f2 -d'=' -s)
+		if [ -n "$patchlevel" ]; then
+			resetprop ro.build.version.security_patch "$patchlevel"
+			sed -i "s/ro.build.version.security_patch=.*/ro.build.version.security_patch=""$patchlevel""/g" "/$DEFAULTPROP" ;
+			log_info "New security patch level: $patchlevel"
+		fi
+	else
+		log_info "Current system is Nougat or older. Skipping OS version and security patch level setting..."
 	fi
 	# Set additional props from build.prop
-	# Only needed for some devices, so this section can be removed if your device isn't one of them
-	log_info "Current device: $device"
-	device=$(grep -i 'ro.product.device' "$TEMPSYS/$BUILDPROP"  | cut -f2 -d'=' -s)
-	if [ -n "$device" ]; then
-		resetprop ro.product.device "$device"
-		sed -i "s/ro.product.device=.*/ro.product.device=""$device""/g" "/$DEFAULTPROP" ;
-		log_info "New device: $device"
+	# Only needed for some devices, so set "SETDEVICE" variable to "false" if your device isn't one of them
+	if [ "$SETDEVICE" = "true" ]; then
+		log_info "Current device: $device"
+		device=$(grep -i 'ro.product.device' "$TEMPSYS/$BUILDPROP"  | cut -f2 -d'=' -s)
+		if [ -n "$device" ]; then
+			resetprop ro.product.device "$device"
+			sed -i "s/ro.product.device=.*/ro.product.device=""$device""/g" "/$DEFAULTPROP" ;
+			log_info "New device: $device"
+		fi
+		log_info "Current product: $product"
+		product=$(grep -i 'ro.build.product' "$TEMPSYS/$BUILDPROP"  | cut -f2 -d'=' -s)
+		if [ -n "$product" ]; then
+			resetprop ro.build.product "$product"
+			sed -i "s/ro.build.product=.*/ro.build.product=""$product""/g" "/$DEFAULTPROP" ;
+			log_info "New product: $product"
+		fi
 	fi
-	log_info "Current fingerprint: $fingerprint"
-	fingerprint=$(grep -i 'ro.build.fingerprint' "$TEMPSYS/$BUILDPROP"  | cut -f2 -d'=' -s)
-	if [ -n "$fingerprint" ]; then
-		resetprop ro.build.fingerprint "$fingerprint"
-		sed -i "s/ro.build.fingerprint=.*/ro.build.fingerprint=""$fingerprint""/g" "/$DEFAULTPROP" ;
-		log_info "New fingerprint: $fingerprint"
-	fi
-	log_info "Current product: $product"
-	product=$(grep -i 'ro.build.product' "$TEMPSYS/$BUILDPROP"  | cut -f2 -d'=' -s)
-	if [ -n "$product" ]; then
-		resetprop ro.build.product "$product"
-		sed -i "s/ro.build.product=.*/ro.build.product=""$product""/g" "/$DEFAULTPROP" ;
-		log_info "New product: $product"
+	# Only needed for some devices (for stock OTA capability), so set "SETFINGERPRINT" variable to "false" if your device isn't one of them
+	if [ "$SETFINGERPRINT" = "true" ]; then
+		log_info "Current fingerprint: $fingerprint"
+		fingerprint=$(grep -i 'ro.build.fingerprint' "$TEMPSYS/$BUILDPROP"  | cut -f2 -d'=' -s)
+		if [ -n "$fingerprint" ]; then
+			resetprop ro.build.fingerprint "$fingerprint"
+			sed -i "s/ro.build.fingerprint=.*/ro.build.fingerprint=""$fingerprint""/g" "/$DEFAULTPROP" ;
+			log_info "New fingerprint: $fingerprint"
+		fi
 	fi
 	finish
 else
